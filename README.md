70f6e3807db8f390c4aa62a08a194fa308b4c343
jianjun.xie <xiejianjun@codemao.cn>
2021-06-08 11:29:33 +0800

    feat: 添加预览视频

---
+++ README.md
@@ -0,0 +1,5 @@
+## 简介
+利用WebRTC API调用摄像头并录制视频
+## 运行
+vscode安装Live Server开启本地服务运行
+* 要在localhost或者https域名下预览
--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
 console.log(adapter.browserDetails.browser)
 console.log(adapter.browserDetails.version)
 
-class Camera {
+class VideoRecorde {
   constructor(liveVideoId, mediaOption = {}, recorderOptions = {}) {
     this.liveVideoEle = document.querySelector(liveVideoId)
-    // 
+    // userMedia选项
     this.mediaOption = {
       audio: true,
       video: {
-        width: 750,
-        height: 350,
+        width: 730,
+        height: 460,
         facingMode: 'user', // 'user' 前置 'environment' 后置
         frameRate: { ideal: 10, max: 15 }, // 帧率
       },
       ...mediaOption
     }
+    // mediaRecorder选项
     this.mediaRecorderOptions = {
       audioBitsPerSecond: 128000,
       videoBitsPerSecond: 2500000,
@@ -24,7 +25,7 @@ class Camera {
   }
   openCamera(mediaOption = {}) {
     navigator.mediaDevices.getUserMedia({...this.mediaOption, ...mediaOption})
-      .then(function(mediaStream) {
+      .then((mediaStream) => {
         this._setLiveVideoSrc(mediaStream);
       })
       .catch(function(err) { console.log(`${err.name }: ${ err.message}`); }); // 总是在最后检查错误
@@ -32,34 +33,26 @@ class Camera {
   closeCamera() {
     this.liveVideoEle.srcObject.getTracks().forEach((track) => { track.stop(); });
   };
-  _setLiveVideoSrc(srcObject) {
+  _setLiveVideoSrc(mediaStream) {
     // 旧的浏览器可能没有srcObject
     if ('srcObject' in this.liveVideoEle) {
-      this.liveVideoEle.srcObject = srcObject;
+      this.liveVideoEle.srcObject = mediaStream;
     } else {
     // 防止在新的浏览器里使用它，应为它已经不再支持了
-      this.liveVideoEle.src = window.URL.createObjectURL(srcObject);
+      this.liveVideoEle.src = window.URL.createObjectURL(mediaStream);
     }
-    this.liveVideoEle.onloadedmetadata = function() {
+    this.liveVideoEle.onloadedmetadata = () => {
       this.liveVideoEle.play();
     };
   }
-  _createMediaRecorder() {
-    this.mediaRecorder = new MediaRecorder(this.liveVideoEle.srcObject, this.mediaRecorderOptions);
-  }
-  startRecorde() {
-    const _this = this
-    this._createMediaRecorder()
-    return new Promise((resolve, reject) => {
-      _this.mediaRecorder = dataAvailable = (blob) => {
-        resolve(blob.data)
-      }
-    })
+  startRecorde(option = {}) {
+    this.mediaRecorder = new MediaRecorder(this.liveVideoEle.srcObject, {...this.mediaRecorderOptions, ...option});
+    this.mediaRecorder.start()
   }
   endRecorde() {
-    console.log(this.mediaRecorder.stop());
+    return new Promise((resolve) => {
+      this.mediaRecorder.ondataavailable =  resolve
+      this.mediaRecorder.stop()
+    })
   }
-  dataAvailable = (blob) => {
-    setPreviewVideoSrc(blob.data);
-  };
 }
